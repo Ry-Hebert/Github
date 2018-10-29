45a558f74c30d9e42ff53be1242c9f3199bee2d9
Ryan <ryan@thecpafirm.com>
2018-10-28 21:54:23 -0600

    Content Clean up

--- main.js
+++ main.js
@@ -2,9 +2,14 @@ let search1 = null;
 let search2 = null;
 let results = null;
 
-myFunction = () => 
+myFunction1 = () => 
 {
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("myDropdown1").classList.toggle("show");
+}
+
+myFunction2 = () => 
+{
+    document.getElementById("myDropdown2").classList.toggle("show");
 }
 
 // Close the dropdown if the user clicks outside of it
@@ -24,4 +29,44 @@ window.onclick = (event) =>
       }
     }
   }
+};
+
+filterFunc = (pram1, pram2) =>
+{
+if(pram1 === 1)
+{
+ search1 = pram2;
+ return;
+} 
+if(pram1 == 2)
+{
+  search2 = pram2;
+  return;
 }
+else{console.error('Error: Your search turned up no results.');}
+};
+
+returnResults = () =>
+{
+  fetch('https://swapi.co/api/'+search1+'/'+search2+'/')
+      .then(function(response)
+      {
+        return response.json();
+      })
+      .then(function(myJson)
+      {
+        search1 = myJson;
+        console.log(JSON.stringify(myJson)); //verify the correct object is being passing via console. 
+      });
+}
+
+//fetch('https://swapi.co/api/'+pram2+'/')
+//    .then(function(response)
+//    {
+//      return response.json();
+//    })
+//    .then(function(myJson)
+//    {
+//      search1 = myJson;
+//      console.log(JSON.stringify(myJson)); //verify the correct object is being passing via console. 
+//    });
