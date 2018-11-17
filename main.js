b0e978ad5b9e4a41bbb4593d3f076de8439a42ff
Ryan <ryan@thecpafirm.com>
2018-11-17 12:24:49 -0700

    File Directory Changes

--- main.js
+++ main.js
@@ -1,74 +1,26 @@
-let search1 = null;
-let search2 = null;
-let results = document.querySelector("#searchResults");
+//https://fortnite-public-api.theapinetwork.com/prod09/upcoming/get
+let searchResults = null;
+let processedResults = null;
+let hasRequested = 0;
 
-myFunction1 = () => 
+myFunction1 = () =>
 {
-    document.getElementById("myDropdown1").classList.toggle("show");
+if(hasRequested === 0)
+{
+    fetch(`https://fortnite-public-api.theapinetwork.com/prod09/upcoming/get`)
+    .then(response =>
+        {
+            searchResults = response;
+        })
+    processedResults = JSON.parse(searchResults.items);
+    hasRequested++;  
 }
 
-myFunction2 = () => 
+console.log(hasRequested);
+
+processedResults.forEach(element => 
 {
-    document.getElementById("myDropdown2").classList.toggle("show");
+    console.log(element.name)
+});
+
 }
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = (event) => 
-{
-  if (!event.target.matches('.dropbtn')) 
-  {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) 
-    {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) 
-      {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-};
-
-filterFunc = (pram1, pram2) =>
-{
-if(pram1 === 1)
-{
- search1 = pram2;
- return;
-} 
-if(pram1 == 2)
-{
-  search2 = pram2;
-  return;
-}
-else{console.error('Error: Your search turned up no results.');}
-};
-
-returnResults = () =>
-{
-  fetch(`https://swapi.co/api/${search1}/${search2}/`)
-      .then(function(response)
-      {
-        return response.json();
-      })
-      .then(function(myJson)
-      {
-        let tryThis = JSON.stringify(myJson);
-        results.appendChild(document.createElement('li')).textContent=tryThis;
-
-        console.log(tryThis); //verify the correct object is being passing via console. 
-      });
-};
-
-//fetch('https://swapi.co/api/'+pram2+'/')
-//    .then(function(response)
-//    {
-//      return response.json();
-//    })
-//    .then(function(myJson)
-//    {
-//      search1 = myJson;
-//      console.log(JSON.stringify(myJson)); //verify the correct object is being passing via console. 
-//    });
