dcc9c53b144e73de9b96e76a27f6d4a53989782a
Ryan <ryan@thecpafirm.com>
2018-12-13 01:14:41 -0700

    Cleaning up code and Documentation

--- assets/main.js
+++ assets/main.js
@@ -1,77 +1,72 @@
-let searchResults = null; //Switch to MF1 Local
+//  Global Variables
+const results = document.querySelector("#api-search-results"); //Shortens the Code needed to input new elements into the HTML
+let customCount = 0; //Stores the number of times a user has input a new item suggestion. 
 
-let hasRequested = 0; //Switch to MF1 Local
-
-const results = document.querySelector("#api-search-results");
-let customCount = 0;
-
-//First Checks if the API has already been queried, If it hasn't then reference it and display information retrieved.
+//  First Checks if the API has already been queried, If it hasn't then reference it and display information retrieved.
 myFunction1 = () =>
 {
-if(hasRequested === 0) //Used to see if the API has been queried already and reduce excessive calls. 
-{
-        fetch(`https://fortnite-public-api.theapinetwork.com/prod09/upcoming/get`)
-        .then(function(response)
-        {
-          return response.json();
-        })
-        .then(function(myJson)
-        {
-            searchResults = myJson;
-            
-            //Trouble Shooting Tools
-            //let tryThis = JSON.stringify(myJson);
-            //console.log(tryThis); //verify the correct object is being passing via console.
-            //console.log(myJson.items.length);
+let searchResults = null;
+let hasRequested = 0;
 
-            // Loop through the length of the returned list and create DOM elements
-            for(let i = 0; i < myJson.items.length; i++)
-                {   
-                    let cImage = myJson.items[i].item.images.background;
-                    let itemShort = myJson.items[i]; //!! Maybe Check to optimize!!
-                    
-                    let itemInfo = 
-                    {
-                       'name': `${itemShort.name}`,
-                       'cost': `${itemShort.cost}`,
-                       'rarity': `${itemShort.item.rarity}`
-                    }
+    //Used to see if the API has been queried already and reduce excessive calls.
+    if(hasRequested === 0)
+    {
+            fetch(`https://fortnite-public-api.theapinetwork.com/prod09/upcoming/get`)
+            .then(function(response)
+            {
+            return response.json();
+            })
+            .then(function(myJson)
+            {
+                searchResults = myJson;
 
-                    //Create Cards Display
-                    results.innerHTML += 
-                    `<div id='card${i}' class='card' onclick="cardFlip(${i})">
-                        <div class='front' style='background-image: url("${cImage}");'></div>
-                        <div class='back' style='background-color: #000;'>
-                            <ul>
-                                <li>${itemInfo.name}</li>
-                                <li>Cost: ${itemInfo.cost}</li>
-                                <li>Rarity: ${itemInfo.rarity}</li>
-                            </ul>
-                        </div>
-                    </div>`;
-                    //results.appendChild(document.createElement('div')).textContent=myJson.items[i].name;
-                    //results.appendChild(document.createElement('img')).textContent=myJson.items[i].name;
-                } 
-        });
-        hasRequested++; //Tell the Function that the API has been queried. 
-}
-//Trouble Shooting Tools
-//console.log(hasRequested);
-//console.log(searchResults);
+                // Loop through the length of the returned list and create DOM elements
+                for(let i = 0; i < myJson.items.length; i++)
+                    {   
+                        let cImage = myJson.items[i].item.images.background;
+                        let itemShort = myJson.items[i];
+
+                        let itemInfo = 
+                        {
+                        'name': `${itemShort.name}`,
+                        'cost': `${itemShort.cost}`,
+                        'rarity': `${itemShort.item.rarity}`
+                        }
+
+                        //Create Cards Display
+                        results.innerHTML += 
+                        `<div id='card${i}' class='card' onclick="cardFlip(${i})">
+                            <div class='front' style='background-image: url("${cImage}");'></div>
+                            <div class='back' style='background-color: #000;'>
+                                <ul>
+                                    <li>${itemInfo.name}</li>
+                                    <li>Cost: ${itemInfo.cost}</li>
+                                    <li>Rarity: ${itemInfo.rarity}</li>
+                                </ul>
+                            </div>
+                        </div>`;
+                        
+                        //results.appendChild(document.createElement('div')).textContent=myJson.items[i].name;
+                    } 
+            });
+            hasRequested++; //Tell the Function that the API has been queried. 
+    }
 }
 
-//Provides functionality for card flipping action.
+//  Provides functionality for card flipping action.
 cardFlip = (i) =>
 {
 document.querySelector(`#card${i}`).classList.toggle('is-flipped');
 }
 
-//User Input Card Generator.
+//  User Input Card Generator.
 formSubmit = () =>
 {
-let formData = document.querySelector('#fAddNewItem');
+//Pull data from html fields.
 let name = document.querySelector('#fItem_name').value;
 let rarity = document.querySelector('#rarity').value;
+
+//Creates Card Display
 results.innerHTML += 
                     `<div id='cardCustom${customCount}' class='card' onclick="cardFlip('Custom${customCount}')">
                         <div class='front' style='background-image: url("assets/question_block.png");'></div>
@@ -83,5 +78,5 @@ results.innerHTML +=
                             </ul>
                         </div>
                     </div>`;
-customCount++;
+customCount++; //Update the number of user elements to keep elements unique.
 }
